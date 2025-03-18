24295bc196edc744fd98247ac8110af9c649a81b
misa <21sbabe@naver.com>
2025-03-19 01:12:32 +0900

    03.19 01:12 edit
    main slide, notice, about edit

--- js/app.js
+++ js/app.js
@@ -15,7 +15,7 @@ document.addEventListener("DOMContentLoaded", function () {
     let autoSlide;
 
     function updateSlidePosition() {
-        slideWrap.style.transform = `translateX(-${currentIndex * 100}%)`;
+        slideWrap.style.left = `-${currentIndex * 100}%`;
         progressBar.style.width = `${((currentIndex + 1) / totalSlides) * 100}%`;
     }
 
@@ -72,4 +72,75 @@ document.addEventListener("DOMContentLoaded", function () {
     slideWrap.addEventListener("mouseleave", startAutoSlide);
 
     updateSlidePosition();
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    //      notice banner
+
+    const ntbWrap = document.querySelector(".ntb_slide_wrap");
+    const ntbSlides = document.querySelectorAll(".ntb_slide");
+    const ntbProgressBar = document.querySelector(".notice_progressbar .progressbar");
+
+    let currentNtbIndex = 0;
+    let totalNtbSlides = ntbSlides.length;
+    let startNtbX = 0;
+    let isNtbDragging = false;
+    let ntbAutoSlide;
+
+    function updateSlidePosition() {
+        ntbWrap.style.left = `-${currentNtbIndex * 100}%`;
+        ntbProgressBar.style.width = `${((currentNtbIndex + 1) / totalNtbSlides) * 100}%`;
+    }
+
+    function nextSlide() {
+        currentNtbIndex = (currentNtbIndex + 1) % totalNtbSlides;
+        updateSlidePosition();
+    }
+
+    function prevSlide() {
+        currentNtbIndex = (currentNtbIndex - 1 + totalNtbSlides) % totalNtbSlides;
+        updateSlidePosition();
+    }
+
+    function startAutoSlide() {
+        ntbAutoSlide = setInterval(nextSlide, 5000);
+    }
+
+    function stopAutoSlide() {
+        clearInterval(ntbAutoSlide);
+    }
+
+    //       drag event ( mouse + touch )
+    ntbWrap.addEventListener("mousedown", (e) => {
+        isNtbDragging = true;
+        startNtbX = e.clientX;
+    });
+
+    ntbWrap.addEventListener("mouseup", (e) => {
+        isNtbDragging = false;
+        let diff = startNtbX - e.clientX;
+        if (diff > 50) nextSlide();
+        else if (diff < -50) prevSlide();
+    });
+
+    ntbWrap.addEventListener("touchstart", (e) => {
+        isNtbDragging = true;
+        startNtbX = e.touches[0].clientX;
+    });
+
+    ntbWrap.addEventListener("touchend", (e) => {
+        isNtbDragging = false;
+        let diff = startNtbX - e.changedTouches[0].clientX;
+        if (diff > 50) nextSlide();
+        else if (diff < -50) prevSlide();
+    });
+
+    //      autoplay ( + stop when mose over or slide )
+    startAutoSlide();
+    ntbWrap.addEventListener("mouseenter", stopAutoSlide);
+    ntbWrap.addEventListener("mouseleave", startAutoSlide);
+
+    updateSlidePosition();
+
 });
