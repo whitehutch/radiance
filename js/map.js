da82f199f5c2f4922bd52987f50d800668d27851
misa <21sbabe@naver.com>
2025-05-12 23:10:23 +0900

    edited 25.05.12 23:10
    map complete, contact open

--- js/map.js
+++ js/map.js
@@ -4,7 +4,7 @@ async function initMap() {
     const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
 
     const map = new google.maps.Map(document.getElementById("map"), {
-        center: { lat: 35.158558, lng: 129.063988 },
+        center: { lat: 35.158648, lng: 129.063918 },
         zoom: 19,
         mapId: 'ed3bc29771171eed'
     });
@@ -16,7 +16,8 @@ async function initMap() {
     mapWrap.className = "marker-wrap";
     mapMarker.className = "map-marker";
     markerTail.className = "marker-tail";
-    mapMarker.textContent = "radiance";
+    // mapMarker.innerHTML = '<span>here<ion-icon name="cafe"></ion-icon></span>';
+    mapMarker.innerHTML = '<ion-icon name="cafe"></ion-icon>';
 
     mapWrap.appendChild(mapMarker);
     mapWrap.appendChild(markerTail);
@@ -26,19 +27,73 @@ async function initMap() {
         position: { lat: 35.158610, lng: 129.064220 },
         content: mapWrap,
     });
+
+    marker.markerWrapElement = mapWrap;
+
+    for (const property of properties) {
+        const markerElement = new google.maps.marker.AdvancedMarkerElement({
+            map,
+            content: buildContent(property),
+            position: property.position,
+            title: property.description,
+        });
+
+        markerElement.markerWrapElement = mapWrap;
+
+        markerElement.addListener("click", () => {
+            toggleHighlight(markerElement, property);
+        });
+    }
 }
 
-function replaceStreetViewIcons() {
-    const interval = setInterval(() => {
-        const svImages = document.querySelectorAll('.gm-svpc > div img');
-        if (svImages.length === 3) {
-            svImages[0].src = '/images/map.svg'; // 첫 번째 이미지
-            svImages[1].src = '/images/map_bin.svg'; // 두 번째 이미지
-            svImages[2].src = '/images/map_fly.svg'; // 세 번째 이미지
-            clearInterval(interval);
-        }
-    }, 100);
+function toggleHighlight(markerView, property) {
+    const markerWrapElement = markerView.markerWrapElement;
+
+    if (markerView.content.classList.contains("highlight")) {
+        markerView.content.classList.remove("highlight");
+        if (markerWrapElement) markerWrapElement.classList.remove("highlight");
+        markerView.zIndex = null;
+    } else {
+        markerView.content.classList.add("highlight");
+        if (markerWrapElement) markerWrapElement.classList.add("highlight");
+        markerView.zIndex = 1;
+    }
 }
+
+function buildContent(property) {
+    const content = document.createElement("div");
+    content.classList.add("property");
+    content.innerHTML = `
+    <div class="highlight">
+        <div class="hl_wrap">
+            <div class="map_image">
+                <img src="${property.images}" alt="">
+            </div>
+            <div class="map_text">
+                <h6 class="map_title">${property.title}</h6>
+                <p class="map_sub">${property.subTitle}</p>
+                <p class="map_add">${property.address}</p>
+                <p class="map_enadd">${property.enAddress}</p>
+            </div>
+        </div>
+    </div>
+    `;
+    return content;
+}
+
+const properties = [
+    {
+        images: "/images/map_img.png",
+        title: "radiance",
+        subTitle: "래디언스",
+        address: "부산광역시 부산진구 서전로 37번길 18 2F",
+        enAddress: "Busanjin-gu Seojeon-rop 37Beon-gil 18 2F",
+        position: {
+            lat: 35.158610,
+            lng: 129.064220,
+        },
+    }
+];
 
 initMap();
 setTimeout(replaceStreetViewIcons, 1000);
